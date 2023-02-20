4abb3bd55d38c3cfbf433ac8ced25e73c25cdd32
James <jamesgrajales5@gmail.com>
2023-02-20 16:13:02 -0500

    Finaliza proyecto

--- src/app.js
+++ src/app.js
@@ -22,3 +22,36 @@
     }
   });
 })(document);
+
+// contact form
+((d) => {
+  const $form = d.querySelector('.contact-form');
+  const $loader = d.querySelector('.contact-form-loader');
+  const $response = d.querySelector('.contact-form-response');
+  $form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    $loader.classList.remove('hiden');
+    fetch('https://formsubmit.co/ajax/jamesgrajales5@gmail.com', {
+      method: 'POST',
+      body: new FormData(e.target),
+    })
+      .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+      .then((json) => {
+        location.hash = '#gracias';
+        $form.reset();
+      })
+      .catch((err) => {
+        console.log(err);
+        let message = err.statusText || 'Ocurrio un errro, intenta nuevamente';
+        $response.querySelector(
+          'h3'
+        ).innerHTML = `Error ${err.status}:${message}`;
+      })
+      .finally(() => {
+        $loader.classList.add('hiden');
+        setTimeout(() => {
+          location.hash = '#close';
+        }, 3000);
+      });
+  });
+})(document);
